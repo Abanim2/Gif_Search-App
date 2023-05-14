2ed2f186f3f0d6228f1f0903ad374d0c6c3a9ebc
Abanim2 <115773506+Abanim2@users.noreply.github.com>
2023-05-15 02:54:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import GifSearch from "./components/GifSearch";
